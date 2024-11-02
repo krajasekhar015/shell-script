ecb06fb3c1eb52c2be05f0c1f9fc2951744b3a0b
krajasekhar015 <krajasekhar015@gmail.com>
2024-11-02 14:39:50 +0530

    shell

---
+++ doc-eks-kube.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo su -
+
+#check whether root user or not
+R="\e[31m"
+N="\e[0m"
+
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ec2-user
+
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.0/2024-09-12/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo mv /tmp/eksctl /usr/local/bin
+
+
+
+echo -e "$R Logout and Login again $N"
